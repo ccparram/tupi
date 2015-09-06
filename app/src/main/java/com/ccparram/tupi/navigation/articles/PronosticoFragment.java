e39f0f5292202584867870a9e30fd5e3a20f6e6b
Camilo Parra <ccparram@unal.edu.co>
2015-09-06 13:05:19 -0500

    JObjecto Json creado, Layout Pronostico listo

--- app/src/main/java/com/ccparram/tupi/navigation/articles/PronosticoFragment.java
+++ app/src/main/java/com/ccparram/tupi/navigation/articles/PronosticoFragment.java
@@ -1,17 +1,30 @@
 package com.ccparram.tupi.navigation.articles;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.ccparram.tupi.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class PronosticoFragment extends Fragment {
     public static final String ARG_ARTICLES_NUMBER = "articles_number";
+
+    private final String TAG = this.getClass().getName();
 
     public PronosticoFragment() {
         // Constructor vacío
@@ -20,14 +33,112 @@ public class PronosticoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_article, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_pronostico, container, false);
         int i = getArguments().getInt(ARG_ARTICLES_NUMBER);
         String article = getResources().getStringArray(R.array.Tags)[i];
 
         getActivity().setTitle(article);
-        TextView headline = (TextView) rootView.findViewById(R.id.headline);
-        headline.append(" " + article);
+
+        ObtenerCLimaTask pronosticoTask = new ObtenerCLimaTask();
+        pronosticoTask.execute();
 
         return rootView;
     }
+
+
+    private class ObtenerCLimaTask extends AsyncTask<Void, Void, Void>{
+
+        String pronosticoJsonStr = null;
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+
+            try {
+
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=medellin,co&units=metric&cnt=2");
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                pronosticoJsonStr = buffer.toString();
+
+                Log.e(TAG, " Json" + pronosticoJsonStr);
+
+            }
+            catch (IOException e) {
+                Log.e(TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            super.onPostExecute(aVoid);
+
+            try {
+                JSONObject forecastJson = new JSONObject(pronosticoJsonStr);
+
+
+
+
+
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+
+    }
+
+
+
+
 }

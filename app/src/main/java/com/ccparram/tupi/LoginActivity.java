2a2bd365687347af59f7a0f09b5e37e58451205d
Camilo Parra <ccparram@unal.edu.co>
2015-09-07 00:17:44 -0500

    Login FB funciona y se obtiene el Auth Token, todo en LoginActivity

--- app/src/main/java/com/ccparram/tupi/LoginActivity.java
+++ app/src/main/java/com/ccparram/tupi/LoginActivity.java
@@ -3,16 +3,30 @@ package com.ccparram.tupi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ccparram.tupi.navigation.Navigation;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getName();
+
+    private TextView info;
+    private LoginButton loginButton;
+
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +35,48 @@ public class LoginActivity extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         // Initialize the SDK before executing any other operations,
         // especially, if you're using Facebook UI elements.
-        setContentView(R.layout.activity_login);
 
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );
+                Log.e(TAG, "Logueado");
+
+                //Intent intent = new Intent(getBaseContext(), Navigation.class);
+                //startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+                Log.e(TAG, "Login cancelado");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+                Log.e(TAG, "Error en login");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
 
